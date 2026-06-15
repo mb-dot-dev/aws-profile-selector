3729138c5262f36e4007cf2513382b6d68d37b6d
Bence Molnár <developer@molnarbence.dev>
2026-06-15 12:37:33 +0200

    chore: scaffold solution, projects, and Spectre packages

---
+++ src/AwsProfileSelector/Program.cs
@@ -0,0 +1,2 @@
+// Entry point is implemented in Task 10.
+return 0;
---
+++ tests/AwsProfileSelector.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AwsProfileSelector.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
